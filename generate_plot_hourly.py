e4d485a971563be510fad7c604d1558fa171cb6a
Jesse Bishop <jessebishop@yahoo.com>
2013-05-29 22:12:36 -0400

    end of night commit

--- generate_plot_hourly.py
+++ generate_plot_hourly.py
@@ -15,7 +15,10 @@ now = datetime.datetime.now()
 # Create hourly plot for last 24 hours
 # Get the data from the round robin table first
 ophour = now.hour - 1
-cursor.execute("""SELECT hour, kwh, kwh_avg, kwh_avg_dow, complete FROM electricity_usage_hourly;""")
+cursor.execute("""SELECT hour, kwh, kwh_avg, kwh_avg_dow, complete FROM electricity_usage_hourly ORDER BY timestamp;""")
+data = cursor.fetchall()
+hour, kwh, kwh_avg, kwh_avg_dow, complete  = zip(*data)
+splitpos = hour.index(ophour)
 
 minutes = datetime.datetime.now().minute + 23 * 60
 cursor.execute("""SELECT SUM(watts * tdiff / 60 / 60 / 1000.) AS kwh, date_part('hour', time) AS hour, to_timestamp(min(date_part('year', time))::text || '/' || min(date_part('month', time))::text || '/' || min(date_part('day', time))::text || ' ' || date_part('hour', time)::text || ':00:00', 'YYYY/MM/DD HH24:MI:SS') AS date  FROM temp_electricity WHERE time > CURRENT_TIMESTAMP - interval '%s minutes' GROUP BY hour ORDER BY date;""" % minutes)
