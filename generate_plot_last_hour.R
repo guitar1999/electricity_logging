67428f0e1e7ffc06daa52bbbadabf56134103ae9
Jesse Bishop <jessebishop@yahoo.com>
2013-06-13 22:00:40 -0400

    speed improvements

--- generate_plot_last_hour.R
+++ generate_plot_last_hour.R
@@ -8,13 +8,15 @@ fname <- '/var/www/electricity/last_hours.png'
 mintime <- min(res$measurement_time)
 maxtime <- max(res$measurement_time)
 maxwatts <- max(res$watts)
+vseq <- seq(0, maxwatts, ifelse(maxwatts > 1000, 200, 100))
+hseq <- seq(mintime, mintime + 7200, 600)
 
 png(filename=fname, width=1024, height=400, units='px', pointsize=12, bg='white')
-plot(res$measurement_time, res$watts, type='l', col='white', xlim=c(mintime, mintime + 7200), ylim=c(0,maxwatts), xlab="Time", ylab="Watts", main=paste("Electricity Usage since ", mintime))
+plot(res$measurement_time, res$watts, type='l', col='white', xlim=c(mintime, mintime + 7200), ylim=c(0,maxwatts), xlab="Time", ylab="Watts", main=paste("Electricity Usage since ", mintime), xaxt='n', yaxt='n')
+axis(side=1, at=hseq, labels=substr(hseq, 12, 16))
+axis(side=2, at=vseq, labels=vseq)
 abline(v=mintime + 3600, col='black')
-for (i in seq(0, maxwatts, ifelse(maxwatts > 1000, 200, 100))){
-    abline(h=i, col='grey', lty=2)
-}
+abline(h=vseq, col='grey', lty=2)
 lines(res$measurement_time, res$watts, col='red')
 dev.off()
 
