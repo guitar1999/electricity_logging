31e35598d241bf93b3ece35d32388bf1ef1bde85
Jesse Bishop <jessebishop@yahoo.com>
2013-06-15 14:38:42 -0400

    fixing label

--- generate_plot_dow.R
+++ generate_plot_dow.R
@@ -2,10 +2,10 @@ library(RPostgreSQL)
 source('/home/jessebishop/scripts/electricity_logging/barplot.R')
 con <- dbConnect(drv="PostgreSQL", host="127.0.0.1", user="jessebishop", dbname="jessebishop")
 
-query <- "SELECT dow, day_of_week, kwh, kwh_avg, complete FROM electricity_usage_dow WHERE NOT dow = date_part('dow', CURRENT_TIMESTAMP) ORDER BY timestamp;"
+query <- "SELECT day_of_week, dow, kwh, kwh_avg, complete FROM electricity_usage_dow WHERE NOT dow = date_part('dow', CURRENT_TIMESTAMP) ORDER BY timestamp;"
 res <- dbGetQuery(con, query)
 
-query2 <- "SELECT date_part('dow', CURRENT_TIMESTAMP) AS dow, day_of_week, akwh AS kwh, kwh_avg, 'no'::text AS complete FROM (SELECT SUM((watts_ch1 + watts_ch2) * tdiff / 60 / 60 / 1000.) AS akwh FROM electricity_measurements WHERE measurement_time > CURRENT_TIMESTAMP - interval '1 day' AND date_part('dow', measurement_time) = date_part('dow', CURRENT_TIMESTAMP)) AS x, electricity_usage_dow WHERE dow = date_part('dow', CURRENT_TIMESTAMP);"
+query2 <- "SELECT day_of_week, date_part('dow', CURRENT_TIMESTAMP) AS dow, akwh AS kwh, kwh_avg, 'no'::text AS complete FROM (SELECT SUM((watts_ch1 + watts_ch2) * tdiff / 60 / 60 / 1000.) AS akwh FROM electricity_measurements WHERE measurement_time > CURRENT_TIMESTAMP - interval '1 day' AND date_part('dow', measurement_time) = date_part('dow', CURRENT_TIMESTAMP)) AS x, electricity_usage_dow WHERE dow = date_part('dow', CURRENT_TIMESTAMP);"
 res2 <- dbGetQuery(con, query2)
 res <- rbind(res, res2)
 #res$col[res$kwh > res$kwh_avg] <- 'rosybrown' #557
