f37eba6449a5e3f5808ff3ea6b165ebfff7f0834
Jesse Bishop <jessebishop@yahoo.com>
2013-09-14 12:39:54 -0400

    changing log plot

--- generate_plot_last_hour.R
+++ generate_plot_last_hour.R
@@ -11,11 +11,13 @@ maxwatts <- max(res$watts)
 if (maxwatts - min(res$watts) < 2400) {
     vseq <- seq(0, maxwatts, ifelse(maxwatts > 1000, 200, 100))
     vlab <- vseq
+    ymin <- 0
 } else {
     vseq <- log10(c(1,10,50,100,250,500,750,1000,2500,5000,7500,10000))
     vlab <- 10^vseq
     maxwatts <- log10(maxwatts)
     res$watts <- log10(res$watts)
+    ymin <- min(res$watts)
 }
 
 hseq <- seq(mintime, mintime + 7200, 600)
@@ -27,7 +29,7 @@ res2 <- dbGetQuery(con, query2)
 
 
 png(filename=fname, width=1024, height=400, units='px', pointsize=12, bg='white')
-plot(res$measurement_time, res$watts, type='l', col='white', xlim=c(mintime, mintime + 7200), ylim=c(0,maxwatts), xlab="Time", ylab="Watts", main=paste("Electricity Usage since ", mintime), xaxt='n', yaxt='n')
+plot(res$measurement_time, res$watts, type='l', col='white', xlim=c(mintime, mintime + 7200), ylim=c(ymin,maxwatts), xlab="Time", ylab="Watts", main=paste("Electricity Usage since ", mintime), xaxt='n', yaxt='n')
 axis(side=1, at=hseq, labels=substr(hseq, 12, 16))
 axis(side=2, at=vseq, labels=vlab, las=1)
 abline(v=mintime + 3600, col='black')
