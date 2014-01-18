52ec3527853f19c6d3d9b036a1bc8c881950e1e5
Jesse Bishop <jessebishop@yahoo.com>
2014-01-18 18:55:26 -0500

    oops

--- create-table-electricity_statistics_monthly.sql
+++ create-table-electricity_statistics_monthly.sql
@@ -1,5 +1,5 @@
-CREATE TABLE energy_statistics.electricity_statistics_month (
-    monthly integer PRIMARY KEY CHECK (monthly >= 1 AND monthly <= 12),
+CREATE TABLE energy_statistics.electricity_statistics_monthly (
+    month integer PRIMARY KEY CHECK (month >= 1 AND month <= 12),
     count integer,
     kwh_avg numeric,
     timestamp timestamp with time zone
