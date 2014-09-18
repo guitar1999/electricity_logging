d0ab429a3f2eeb4e175df222b95cb931b7dbd288
Jesse Bishop <jessebishop@yahoo.com>
2014-09-18 10:20:44 -0400

    adding columns

--- create-table-electricity_statistics_doy.sql
+++ create-table-electricity_statistics_doy.sql
@@ -2,5 +2,7 @@ CREATE TABLE energy_statistics.electricity_statistics_doy (
     doy integer PRIMARY KEY CHECK (doy >= 1 AND doy <= 366),
     count integer,
     kwh_avg numeric,
+    previous_year numeric,
+    current_year numeric,
     timestamp timestamp with time zone
 );
