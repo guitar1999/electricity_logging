2405c7b27fc94f4343af8b666081691b4546182c
Jesse Bishop <jessebishop@yahoo.com>
2014-09-19 11:22:38 -0400

    moving away from doy to month-day

--- create-table-electricity_statistics_doy.sql
+++ create-table-electricity_statistics_doy.sql
@@ -1,5 +1,8 @@
 CREATE TABLE energy_statistics.electricity_statistics_doy (
     doy integer PRIMARY KEY CHECK (doy >= 1 AND doy <= 366),
+    doy_noleap integer CHECK (doy_noleap >=1 AND doy_noleap <= 365),
+    month integer CHECK (month >= 1 AND month <= 12),
+    day integer CHECK (day >=1 AND day <= 31),
     count integer,
     kwh_avg numeric,
     previous_year numeric,
--- create-table-electricity_usage_doy.sql
+++ create-table-electricity_usage_doy.sql
@@ -1,5 +1,8 @@
 CREATE TABLE electricity_usage_doy (
     doy integer PRIMARY KEY CHECK (doy > 0 AND doy <= 366),
+    doy_noleap integer CHECK (doy_noleap >=1 AND doy_noleap <= 365),
+    month integer CHECK (month >= 1 AND month <= 12),
+    day integer CHECK (day >=1 AND day <= 31),
     kwh numeric,
     kwh_avg numeric,
     complete text CHECK (complete = 'yes' OR complete = 'no'),
