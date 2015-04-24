c94d939743914f69fbbbc67358f6109ee3a1f7f1
Jesse Bishop <jessebishop@yahoo.com>
2015-04-24 13:06:25 -0400

    first changes

--- generate_plot_month_to_month.R
+++ generate_plot_month_to_month.R
@@ -3,19 +3,19 @@ if (! 'package:RPostgreSQL' %in% search()) {
     con <- dbConnect(drv="PostgreSQL", host="127.0.0.1", user="jessebishop", dbname="jessebishop")
 }
 
-query <- "SELECT (watts_ch1 + watts_ch2) * tdiff / 1000. / 60 / 60 AS kwh, measurement_time - interval '1 year' AS measurement_time, sum((watts_ch1 + watts_ch2) * tdiff / 1000. / 60 / 60) OVER (ORDER BY measurement_time) AS cumulative_kwh FROM electricity_measurements WHERE date_part('month', measurement_time) = date_part('month', CURRENT_TIMESTAMP) AND date_part('year', measurement_time) = date_part('year', CURRENT_TIMESTAMP) ORDER BY measurement_time;"
-current_year <- dbGetQuery(con, query)
+#query <- "SELECT (watts_ch1 + watts_ch2) * tdiff / 1000. / 60 / 60 AS kwh, measurement_time - interval '1 year' AS measurement_time, sum((watts_ch1 + watts_ch2) * tdiff / 1000. / 60 / 60) OVER (ORDER BY measurement_time) AS cumulative_kwh FROM electricity_measurements WHERE date_part('month', measurement_time) = date_part('month', CURRENT_TIMESTAMP) AND date_part('year', measurement_time) = date_part('year', CURRENT_TIMESTAMP) ORDER BY measurement_time;"
+#current_year <- dbGetQuery(con, query)
 
-query <- "SELECT kwh, measurement_time, cumulative_kwh FROM electricity_month_previous_year ORDER BY measurement_time;"
-last_year <- dbGetQuery(con, query)
+#query <- "SELECT kwh, measurement_time, cumulative_kwh FROM electricity_month_previous_year ORDER BY measurement_time;"
+#last_year <- dbGetQuery(con, query)
 
 query <- "SELECT kwh_avg FROM electricity_statistics_monthly WHERE month = date_part('month', CURRENT_TIMESTAMP);"
 kwhavg <- dbGetQuery(con, query)
 
-query <- "SELECT time - interval '1 year' AS time, minute FROM prediction_test WHERE date_part('month', time) = date_part('month', current_timestamp) AND minute > 0 ORDER BY time;"
-prediction <- dbGetQuery(con, query)
-prediction <- rbind(prediction, setNames(data.frame(max(last_year$measurement_time), prediction$minute[length(prediction$minute)]), names(prediction)))
-predline <- rbind(current_year[dim(current_year)[1],2:3], setNames(data.frame(prediction[dim(prediction)[1],]), names(current_year)[2:3]))
+#query <- "SELECT time - interval '1 year' AS time, minute FROM prediction_test WHERE date_part('month', time) = date_part('month', current_timestamp) AND minute > 0 ORDER BY time;"
+#prediction <- dbGetQuery(con, query)
+#prediction <- rbind(prediction, setNames(data.frame(max(last_year$measurement_time), prediction$minute[length(prediction$minute)]), names(prediction)))
+#predline <- rbind(current_year[dim(current_year)[1],2:3], setNames(data.frame(prediction[dim(prediction)[1],]), names(current_year)[2:3]))
 
 fname <- '/var/www/electricity/month_to_month.png'
 pmax <- max(c(last_year$cumulative_kwh, current_year$cumulative_kwh, prediction$minute))
