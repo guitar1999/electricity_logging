b4304bdb4a8c070b317928a48639e75564897d8d
Jesse Bishop <guitar1999@aol.com>
2016-03-23 20:18:18 -0400

    fix path

--- plotting/electricity_plot_daemon_launcher.sh
+++ plotting/electricity_plot_daemon_launcher.sh
@@ -2,6 +2,6 @@
 
 while [ 1 ]
 do 
-    /usr/bin/R --vanilla --slave < /usr/local/electricity_logging/electricity_plot_daemon_worker.R
+    /usr/bin/R --vanilla --slave < /usr/local/electricity_logging/plotting/electricity_plot_daemon_worker.R
     echo "Failed at $(date)"
 done
