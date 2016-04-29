03f16a5a510a54c22bc787681f4f72e768587d4f
Jesse Bishop <jessebishop@yahoo.com>
2016-04-28 21:04:53 -0400

    worker

--- db_inserter.py
+++ db_inserter.py
@@ -1,16 +1,22 @@
 from celery import Celery
-import psycopg2
+import ConfigParser, psycopg2
 
-app = Celery('db_inserter', broker='amqp://localhost')
+app = Celery('db_inserter', broker='amqp://energy@localhost')
 
 @app.task
-def into_db(x):
+def insert_electric(watts_ch1, watts_ch2, watts_ch3, readtime, time):
     success = False
     while not success:
         try:
-            db = psycopg2.connect(host='localhost', database='jbishop', user='jbishop')
+            config = ConfigParser.RawConfigParser()
+            config.read('/home/jessebishop/.pyconfig')
+            dbhost = config.get('pidb', 'DBHOST')
+            dbname = config.get('pidb', 'DBNAME')
+            dbuser = config.get('pidb', 'DBUSER')
+            dbport = config.get('pidb', 'DBPORT')
+            db = psycopg2.connect(host=dbhost, port=dbport, database=dbname, user=dbuser)
             cursor = db.cursor()
-            query = "INSERT INTO celery_test (id, ts) VALUES ({0}, CURRENT_TIMESTAMP);".format(x)
+            query = """INSERT INTO electricity_measurements (watts_ch1, watts_ch2, watts_ch3, measurement_time, device_time) VALUES ({0}, {1}, {2}, '{3}', '{4}');""".format(watts_ch1, watts_ch2, watts_ch3, readtime, time)
             cursor.execute(query)
             db.commit()
             cursor.close()
@@ -20,3 +26,25 @@ def into_db(x):
         else:
             success = True
 
+@app.task
+def insert_temperature(temperature, device):
+    success = False
+    while not success:
+        try:
+            config = ConfigParser.RawConfigParser()
+            config.read('/home/jessebishop/.pyconfig')
+            dbhost = config.get('pidb', 'DBHOST')
+            dbname = config.get('pidb', 'DBNAME')
+            dbuser = config.get('pidb', 'DBUSER')
+            dbport = config.get('pidb', 'DBPORT')
+            db = psycopg2.connect(host=dbhost, port=dbport, database=dbname, user=dbuser)
+            cursor = db.cursor()
+            query = """INSERT INTO temperature_test (temperature, device_id) VALUES ({0}, '{1}');""".format(temperature, device)
+            cursor.execute(query)
+            db.commit()
+            cursor.close()
+            db.close()
+        except Exception, msg:
+            success = False
+        else:
+            success = True
