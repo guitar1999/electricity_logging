7ee6827989c6155aa1805a244de34cc4b73b13a0
Jesse Bishop <jessebishop@yahoo.com>
2016-05-14 10:35:50 -0400

    cmp sums

--- schema/create-view-cmp_electricity_sums_hourly_view.sql
+++ schema/create-view-cmp_electricity_sums_hourly_view.sql
@@ -1,9 +1,9 @@
-CREATE VIEW electricity_statistics.electricity_sums_hourly_cmp_view AS (
+CREATE VIEW electricity_cmp.cmp_electricity_sums_hourly_view AS (
     SELECT 
         start_date::date AS sum_date,
         date_part('hour', start_date) AS hour,
         kwh
     FROM 
-        electricity_statistics.electricity_sums_hourly_cmp
+        electricity_cmp.cmp_electricity_sums_hourly
     ORDER BY 1
 );
