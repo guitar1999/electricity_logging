4ecaa6c6fed8c105497fdd503ed6ff16b6009e44
Jesse Bishop <jessebishop@yahoo.com>
2016-06-03 16:23:52 -0400

    changing source to cmp

--- plotting/generate_plot_daily.R
+++ plotting/generate_plot_daily.R
@@ -5,13 +5,20 @@ if (! 'package:RPostgreSQL' %in% search()) {
 
 source('/usr/local/electricity_logging/plotting/barplot.R')
 
-query <- "SELECT to_char(to_timestamp(u.month::text, 'MM'), 'Mon') || '-' || to_char(u.day, '09') AS label, u.kwh, s.previous_year AS kwh_avg, u.complete FROM electricity_usage_doy u INNER JOIN electricity_statistics_doy s ON u.month=s.month AND u.day=s.day WHERE (NOT u.month = date_part('month', CURRENT_TIMESTAMP) OR NOT u.day = date_part('day', CURRENT_TIMESTAMP)) AND u.updated >= CURRENT_TIMESTAMP - interval '29 days' AND NOT u.updated IS NULL ORDER BY u.updated;"
+#query <- "SELECT to_char(to_timestamp(u.month::text, 'MM'), 'Mon') || '-' || to_char(u.day, '09') AS label, u.kwh, s.previous_year AS kwh_avg, u.complete FROM electricity_usage_doy u INNER JOIN electricity_statistics_doy s ON u.month=s.month AND u.day=s.day WHERE (NOT u.month = date_part('month', CURRENT_TIMESTAMP) OR NOT u.day = date_part('day', CURRENT_TIMESTAMP)) AND u.updated >= CURRENT_TIMESTAMP - interval '29 days' AND NOT u.updated IS NULL ORDER BY u.updated;"
+#res <- dbGetQuery(con, query)
+
+#query2 <- "SELECT to_char(to_timestamp(u.month::text, 'MM'), 'Mon') || '-' || to_char(u.day, '09') AS label, increment_usage('electricity_usage_doy', CASE WHEN is_leapyear(date_part('year', CURRENT_TIMESTAMP)::integer) THEN 'doy_leap' ELSE 'doy_noleap' END) AS kwh, previous_year AS kwh_avg, complete FROM electricity_usage_doy u INNER JOIN electricity_statistics_doy s ON u.month=s.month AND u.day=s.day WHERE u.month = date_part('month', CURRENT_TIMESTAMP) AND u.day = date_part('day', CURRENT_TIMESTAMP);"
+#res2 <- dbGetQuery(con, query2)
+
+#res <- rbind(res, res2)
+
+# New Full Query
+#SELECT s.sum_date, s.dow, s.kwh, st.kwh_avg, st.count, s1.kwh AS last_year FROM cmp_electricity_sums_daily_view s INNER JOIN cmp_electricity_statistics_doy st ON DATE_PART('month', s.sum_date) = st.month AND DATE_PART('day', s.sum_date) = st.day LEFT JOIN cmp_electricity_sums_daily_view s1 ON DATE_PART('year', s.sum_date) - 1 = DATE_PART('year', s1.sum_date) AND DATE_PART('month', s.sum_date) = DATE_PART('month', s1.sum_date) AND DATE_PART('day', s.sum_date) = DATE_PART('day', s1.sum_date) ORDER BY s.sum_date DESC LIMIT 30;
+
+query <- "SELECT to_char(to_timestamp(DATE_PART('month', s.sum_date)::text, 'MM'), 'Mon') || '-' || to_char(DATE_PART('day', s.sum_date), '09') AS label, s.kwh, s1.kwh AS kwh_avg, 'Yes' AS complete FROM cmp_electricity_sums_daily_view s LEFT JOIN cmp_electricity_sums_daily_view s1 ON DATE_PART('year', s.sum_date) - 1 = DATE_PART('year', s1.sum_date) AND DATE_PART('month', s.sum_date) = DATE_PART('month', s1.sum_date) AND DATE_PART('day', s.sum_date) = DATE_PART('day', s1.sum_date) ORDER BY s.sum_date DESC LIMIT 30;"
 res <- dbGetQuery(con, query)
 
-query2 <- "SELECT to_char(to_timestamp(u.month::text, 'MM'), 'Mon') || '-' || to_char(u.day, '09') AS label, increment_usage('electricity_usage_doy', CASE WHEN is_leapyear(date_part('year', CURRENT_TIMESTAMP)::integer) THEN 'doy_leap' ELSE 'doy_noleap' END) AS kwh, previous_year AS kwh_avg, complete FROM electricity_usage_doy u INNER JOIN electricity_statistics_doy s ON u.month=s.month AND u.day=s.day WHERE u.month = date_part('month', CURRENT_TIMESTAMP) AND u.day = date_part('day', CURRENT_TIMESTAMP);"
-res2 <- dbGetQuery(con, query2)
-
-res <- rbind(res, res2)
 res$jday <- res$label # Fake it until I fix barplot function to take las as an argument
 
 fname <- '/var/www/electricity/daily.png'
