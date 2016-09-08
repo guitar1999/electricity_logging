5a2dabda5fc61a47ce75f54d87498117230e5a83
Jesse Bishop <jessebishop@yahoo.com>
2016-09-08 17:23:05 -0400

    left join on season since we might not have stats yet

--- plotting/generate_plot_dow_season.R
+++ plotting/generate_plot_dow_season.R
@@ -5,7 +5,7 @@ if (! 'package:RPostgreSQL' %in% search()) {
 
 source('/usr/local/electricity_logging/plotting/barplot.R')
 
-query <- "WITH dates AS (SELECT generate_series::DATE AS d FROM generate_series(CURRENT_TIMESTAMP, CURRENT_TIMESTAMP - interval '6 days', '-1 day')) SELECT u.day_of_week AS label, u.dow, CASE WHEN c.kwh IS NULL THEN u.kwh ELSE c.kwh END AS kwh, s.kwh_avg, CASE WHEN c.kwh IS NULL THEN 'no'::TEXT ELSE 'yes'::TEXT END AS complete FROM dates o INNER JOIN electricity.electricity_usage_dow u ON DATE_PART('dow', o.d)=u.dow LEFT JOIN electricity_cmp.cmp_electricity_sums_daily_view c ON c.sum_date=o.d INNER JOIN weather_data.meteorological_season m ON DATE_PART('doy', o.d)=m.doy INNER JOIN electricity_cmp.cmp_electricity_statistics_dow s ON u.dow=s.dow AND m.season=s.season ORDER BY o.d;"
+query <- "WITH dates AS (SELECT generate_series::DATE AS d FROM generate_series(CURRENT_TIMESTAMP, CURRENT_TIMESTAMP - interval '6 days', '-1 day')) SELECT u.day_of_week AS label, u.dow, CASE WHEN c.kwh IS NULL THEN u.kwh ELSE c.kwh END AS kwh, s.kwh_avg, CASE WHEN c.kwh IS NULL THEN 'no'::TEXT ELSE 'yes'::TEXT END AS complete FROM dates o INNER JOIN electricity.electricity_usage_dow u ON DATE_PART('dow', o.d)=u.dow LEFT JOIN electricity_cmp.cmp_electricity_sums_daily_view c ON c.sum_date=o.d INNER JOIN weather_data.meteorological_season m ON DATE_PART('doy', o.d)=m.doy LEFT JOIN electricity_cmp.cmp_electricity_statistics_dow s ON u.dow=s.dow AND m.season=s.season ORDER BY o.d;"
 res <- dbGetQuery(con, query)
 
 
