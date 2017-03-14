52e454ce35c4698fc8674a10a74b8ce3729688cb
Jesse Bishop <jessebishop@yahoo.com>
2017-03-14 21:02:04 +0000

    all stats in one view

--- schema/create-view-view_hourly.sql
+++ schema/create-view-view_hourly.sql
@@ -1,35 +1,35 @@
 CREATE OR REPLACE VIEW electricity.view_hourly AS (
-    (SELECT
+    SELECT
         row_number() OVER (ORDER BY u.updated) AS row_number,
         u.hour AS label,
         COALESCE(u.kwh, 0) AS kwh,
         s.kwh_avg,
+        ss.kwh_avg AS kwh_avg_season,
+        sd.kwh_avg AS kwh_avg_dow,
+        sds.kwh_avg AS kwh_avg_dow_season,
         u.complete
     FROM
-        electricity.electricity_usage_hourly u
-        INNER JOIN electricity_statistics.electricity_statistics_hourly_season s
+        electricity_usage_hourly u
+        INNER JOIN electricity_statistics_hourly s
             ON u.hour=s.hour
-            AND s.season =
+        INNER JOIN electricity_statistics.electricity_statistics_hourly_season ss
+            ON u.hour=ss.hour
+            AND ss.season =
                 CASE
                     WHEN u.hour > date_part('hour', CURRENT_TIMESTAMP) THEN (SELECT season FROM meteorological_season WHERE doy = date_part('doy', (CURRENT_TIMESTAMP - interval '1 day')))
                     ELSE (SELECT season FROM meteorological_season WHERE doy = date_part('doy', CURRENT_TIMESTAMP))
                 END
-    WHERE
-        NOT u.hour = date_part('hour', CURRENT_TIMESTAMP)
+        INNER JOIN electricity_statistics.electricity_statistics_hourly_dow sd
+            ON u.hour=sd.hour
+            AND sd.dow =
+                CASE
+                    WHEN u.hour > date_part('hour', CURRENT_TIMESTAMP) THEN date_part('dow', (CURRENT_TIMESTAMP - interval '1 day'))
+                    ELSE date_part('dow', CURRENT_TIMESTAMP)
+                END
+        INNER JOIN electricity_statistics.electricity_statistics_hourly_dow_season sds
+            ON u.hour=sds.hour
+            AND sds.dow = sd.dow
+            AND sds.season = ss.season
     ORDER BY
-        u.updated)
-    UNION
-    (SELECT
-        24 AS row_number,
-        date_part('hour', CURRENT_TIMESTAMP) AS label,
-        u.kwh,
-        s.kwh_avg,
-        complete
-    FROM
-        electricity.electricity_usage_hourly u
-        INNER JOIN electricity_statistics.electricity_statistics_hourly_season s
-            ON u.hour=s.hour
-    WHERE
-        u.hour = date_part('hour', CURRENT_TIMESTAMP)
-        AND s.season = (SELECT season FROM meteorological_season WHERE doy = date_part('doy', CURRENT_TIMESTAMP)))
+        u.updated
 );
