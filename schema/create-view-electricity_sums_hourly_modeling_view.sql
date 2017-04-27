d540b93f358dff872949d4210f3911f9d7b258b6
Jesse Bishop <jessebishop@yahoo.com>
2017-04-27 00:48:41 +0000

    new view

--- schema/create-view-electricity_sums_hourly_modeling_view.sql
+++ schema/create-view-electricity_sums_hourly_modeling_view.sql
@@ -3,7 +3,8 @@ CREATE OR REPLACE VIEW electricity_statistics.electricity_sums_hourly_modeling_v
         measurement_time::DATE AS sum_date, 
         DATE_PART('hour', measurement_time) AS hour, 
         SUM(watts_ch1 * tdiff / 1000 / 60 / 60) AS kwh_ch1,
-        SUM(watts_ch2 * tdiff / 1000 / 60 / 60) AS kwh_ch2
+        SUM(watts_ch2 * tdiff / 1000 / 60 / 60) AS kwh_ch2,
+        SUM(watts_ch3 * tdiff / 1000 / 60 / 60) AS kwh_ch3
     FROM 
         electricity_measurements 
     GROUP BY 
