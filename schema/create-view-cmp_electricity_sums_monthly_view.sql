3af1eb7ca5d86e7d2f11a0ac0762ba3ede46851f
Jesse Bishop <jessebishop@yahoo.com>
2017-05-29 21:21:12 -0400

    fix dst

--- schema/create-view-cmp_electricity_sums_monthly_view.sql
+++ schema/create-view-cmp_electricity_sums_monthly_view.sql
@@ -1,16 +1,21 @@
 CREATE OR REPLACE VIEW electricity_cmp.cmp_electricity_sums_monthly_view AS (
-    SELECT 
+    SELECT
         date_part('year', start_date) AS year,
         date_part('month', start_date) AS month,
         sum(kwh) AS kwh,
-        CASE 
-            WHEN CASE WHEN DATE_PART('month', start_date) = 3 THEN COUNT(*) + 1 ELSE COUNT(*) END / 24. = num_days(date_part('year', start_date)::INTEGER, date_part('month', start_date)::INTEGER) THEN 'yes' 
-            ELSE 'no' 
+        CASE
+            WHEN
+                CASE
+                    WHEN DATE_PART('month', start_date) = 3 THEN COUNT(*) + 1
+                    WHEN DATE_PART('month', start_date) = 11 THEN COUNT(*) - 1
+                    ELSE COUNT(*)
+                END / 24. = num_days(date_part('year', start_date)::INTEGER, date_part('month', start_date)::INTEGER) THEN 'yes'
+            ELSE 'no'
         END AS complete
-    FROM 
+    FROM
         electricity_cmp.cmp_electricity_sums_hourly
-    GROUP BY 
+    GROUP BY
         1, 2
-    ORDER BY 
+    ORDER BY
         1, 2
 );
