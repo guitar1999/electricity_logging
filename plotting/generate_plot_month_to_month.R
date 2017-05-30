8be4da23b5a5cf73c9f5b84ba2cba917e386c312
Jesse Bishop <jessebishop@yahoo.com>
2017-05-29 21:01:38 -0400

    month to month test

--- plotting/generate_plot_month_to_month.R
+++ plotting/generate_plot_month_to_month.R
@@ -3,25 +3,25 @@ if (! 'package:RPostgreSQL' %in% search()) {
     source('/home/jessebishop/.rconfig.R')
 }
 
-query <- "SELECT DATE_PART('year', date) AS year, DATE_PART('month', date) AS month, DATE_PART('day', date) AS day, hour, (date || ' ' || hour || ':59:59')::timestamp AS timestamp, ((date + ((DATE_PART('year', CURRENT_TIMESTAMP - interval '4 hours') - DATE_PART('year', date))::integer || ' year')::interval)::date || ' ' || hour || ':59:59')::timestamp AS plotstamp, kwh, SUM(kwh) OVER (PARTITION BY date_part('year', date) ORDER BY date_part('day', date), hour) AS cumulative_kwh FROM electricity_sums_hourly WHERE DATE_PART('month', date) = DATE_PART('month', CURRENT_TIMESTAMP - interval '4 hours') ORDER BY DATE_PART('year', date), DATE_PART('day', date), hour;"
+query <- "SELECT DATE_PART('year', sum_date) AS year, DATE_PART('month', sum_date) AS month, DATE_PART('day', sum_date) AS day, hour, (sum_date || ' ' || hour || ':59:59')::timestamp AS timestamp, ((sum_date + ((DATE_PART('year', CURRENT_TIMESTAMP) - DATE_PART('year', sum_date))::integer || ' year')::interval)::date || ' ' || hour || ':59:59')::timestamp AS plotstamp, kwh, SUM(kwh) OVER (PARTITION BY date_part('year', sum_date) ORDER BY date_part('day', sum_date), hour) AS cumulative_kwh FROM electricity_statistics.electricity_sums_hourly_best_available WHERE DATE_PART('month', sum_date) = DATE_PART('month', CURRENT_TIMESTAMP) ORDER BY DATE_PART('year', sum_date), DATE_PART('day', sum_date), hour;"
 measurements <- dbGetQuery(con, query)
 
-#query <- "SELECT DATE_TRUNC('month', CURRENT_TIMESTAMP - interval '4 hours') AS xmin;"
+#query <- "SELECT DATE_TRUNC('month', CURRENT_TIMESTAMP) AS xmin;"
 #xmin <- dbGetQuery(con, query)$xmin
-#query <- "SELECT DATE_TRUNC('month', CURRENT_TIMESTAMP - interval '4 hours' + interval '1 month') - interval '1 second' AS xmax;"
+#query <- "SELECT DATE_TRUNC('month', CURRENT_TIMESTAMP + interval '1 month') - interval '1 second' AS xmax;"
 #xmax <- dbGetQuery(con, query)$xmax
 xmin <- min(measurements$plotstamp)
 xmax <- max(measurements$plotstamp)
 pxmin <- max(measurements$timestamp)
 
 
-query <- "SELECT kwh_avg FROM electricity_statistics_monthly WHERE month = date_part('month', CURRENT_TIMESTAMP - interval '4 hours');"
+query <- "SELECT kwh_avg FROM cmp_electricity_statistics_monthly WHERE month = date_part('month', CURRENT_TIMESTAMP);"
 kwhavg <- dbGetQuery(con, query)
 
-query <- "SELECT time, CASE WHEN minuteh IS NULL THEN minute ELSE minuteh END AS minute FROM prediction_test WHERE date_part('year', time) = date_part('year', CURRENT_TIMESTAMP - interval '4 hours') AND date_part('month', time) = date_part('month', CURRENT_TIMESTAMP - interval '4 hours') AND minute > 0 ORDER BY time;"
-prediction <- dbGetQuery(con, query)
-prediction <- rbind(prediction, setNames(data.frame(xmax, prediction$minute[length(prediction$minute)]), names(prediction)))
-predline <- rbind(measurements[dim(measurements)[1],c("timestamp", "cumulative_kwh")], setNames(data.frame(prediction[dim(prediction)[1],]), c(names(measurements)[5], names(measurements)[8])))
+# query <- "SELECT time, CASE WHEN minuteh IS NULL THEN minute ELSE minuteh END AS minute FROM prediction_test WHERE date_part('year', time) = date_part('year', CURRENT_TIMESTAMP) AND date_part('month', time) = date_part('month', CURRENT_TIMESTAMP) AND minute > 0 ORDER BY time;"
+# prediction <- dbGetQuery(con, query)
+# prediction <- rbind(prediction, setNames(data.frame(xmax, prediction$minute[length(prediction$minute)]), names(prediction)))
+# predline <- rbind(measurements[dim(measurements)[1],c("timestamp", "cumulative_kwh")], setNames(data.frame(prediction[dim(prediction)[1],]), c(names(measurements)[5], names(measurements)[8])))
 
 fname <- '/var/www/electricity/month_to_month.png'
 ymax <- max(c(measurements$cumulative_kwh, prediction$minute))
@@ -41,8 +41,8 @@ for (i in seq(1, length(years))){
     }
     lines(plotdata$plotstamp, plotdata$cumulative_kwh, col=linecolor, lwd=1.5)
 }
-lines(prediction$time, prediction$minute, col='blue4', lty=2)
-lines(predline, col='darkred', lty=2, lwd=1.5)
+# lines(prediction$time, prediction$minute, col='blue4', lty=2)
+# lines(predline, col='darkred', lty=2, lwd=1.5)
 abline(h=kwhavg, col='orange')
 leg.txt <- c(years[1], '. . .', years[ghostyears], years[length(years)], "predicted total kwh", "average kwh")
 leg.lty <- c(1, 1, 1, 1, 2, 1)
@@ -50,4 +50,4 @@ leg.col <- c(ghostcolors[1], 'white', ghostcolors[ghostyears], 'red', 'blue4', '
 legend("bottomright", legend=leg.txt, col=leg.col, lty=leg.lty, inset=0.01)
 dev.off()
 
-system(paste("scp", fname, paste(webhost, "207.38.86.222:/home/jessebishop/webapps/htdocs/home/frompi/electricity2/", sep=""), sep=' '),ignore.stdout=TRUE,ignore.stderr=TRUE)
+system(paste("scp", fname, paste(webhost, ":/home/jessebishop/webapps/htdocs/home/frompi/electricity2/", sep=""), sep=' '),ignore.stdout=TRUE,ignore.stderr=TRUE)
