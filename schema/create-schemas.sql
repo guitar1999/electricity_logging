94dc91bf3fc7ff561c773fe41850e558fe74ad0c
Jesse Bishop <jessebishop@yahoo.com>
2017-10-04 21:48:27 -0400

    schema changes

--- schema/create-schemas.sql
+++ schema/create-schemas.sql
@@ -4,10 +4,11 @@ CREATE SCHEMA electricity_cmp;
 CREATE SCHEMA electricity_plotting;
 CREATE SCHEMA oil;
 CREATE SCHEMA oil_statistics;
+CREATE SCHEMA oil_plotting;
 CREATE SCHEMA propane;
 CREATE SCHEMA propane_statistics;
 CREATE SCHEMA water;
 CREATE SCHEMA water_statistics;
 CREATE SCHEMA weather_data;
-ALTER DATABASE lunt_energy SET search_path = public, electricity, electricity_statistics, electricity_cmp, electricity_plotting, oil, oil_statistics, propane, propane_statistics, water, water_statistics, weather_data;
+ALTER DATABASE lunt_energy SET search_path = public, electricity, electricity_statistics, electricity_cmp, electricity_plotting, oil, oil_statistics, oil_plotting, propane, propane_statistics, water, water_statistics, weather_data;
 
