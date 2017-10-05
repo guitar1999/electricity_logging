edb1e217e770812b52b2c479496f2bd4ef56cce2
Jesse Bishop <jessebishop@yahoo.com>
2017-10-04 20:27:52 -0400

    fix order

--- schema/create-view-cmp_electricity_sums_hourly_view.sql
+++ schema/create-view-cmp_electricity_sums_hourly_view.sql
@@ -1,9 +1,9 @@
-CREATE VIEW electricity_cmp.cmp_electricity_sums_hourly_view AS (
-    SELECT 
+CREATE OR REPLACE VIEW electricity_cmp.cmp_electricity_sums_hourly_view AS (
+    SELECT
         start_date::date AS sum_date,
         date_part('hour', start_date) AS hour,
         kwh
-    FROM 
+    FROM
         electricity_cmp.cmp_electricity_sums_hourly
-    ORDER BY 1
+    ORDER BY 1, 2
 );
