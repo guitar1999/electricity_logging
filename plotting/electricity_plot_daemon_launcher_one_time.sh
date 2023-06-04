40bb28d9fe8e4eeeb4d9d2737d9df33041557796
Jesse Bishop <jessebishop@yahoo.com>
2023-06-04 19:46:11 -0400

    remove path to R

--- plotting/electricity_plot_daemon_launcher_one_time.sh
+++ plotting/electricity_plot_daemon_launcher_one_time.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/local/bin/R --vanilla --slave < ${HOME}/git/electricity_logging/plotting/electricity_plot_daemon_worker_one_time.R
+R --vanilla --slave < ${HOME}/git/electricity_logging/plotting/electricity_plot_daemon_worker_one_time.R
