0e2e8bde9f3a88fc0f9a63c00973c0fd228a9751
Jesse Bishop <jessebishop@yahoo.com>
2023-11-03 16:58:49 -0400

    examples

---
+++ examples/EXAMPLE.rconfig.R
@@ -0,0 +1,7 @@
+con <- dbConnect(drv="PostgreSQL", host="", port=, user="", dbname="")
+dbhost <- ""
+webhost <- "" # For scping png files to webserver
+webuser <- "" # For scping png files to webserver
+webpath <- "" # For scping png files to webserver
+githome <- "" # The local path to your git repos
+par(family = "Arial")
