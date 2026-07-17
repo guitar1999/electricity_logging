961b96b36dee0b725a682be0aa252ed8b6f736fe
Jesse Bishop <jessebishop@yahoo.com>
2026-07-17 11:48:23 -0400

    fix view

--- schema/create-view-water_statistics_daily_cycle_view.sql
+++ schema/create-view-water_statistics_daily_cycle_view.sql
@@ -34,19 +34,115 @@ CREATE OR REPLACE VIEW water_statistics.water_statistics_daily_cycle_view AS (
         d.total_runtime,
         d.avg_cycle_time,
         dm.median_cycle_time,
-        -- rolling averages over prior N days (including current)
-        AVG(d.avg_cycle_time) OVER (
+        SUM(d.total_cycles) OVER (
+            ORDER BY d.sum_date::TIMESTAMP
+            RANGE BETWEEN '6 days'::INTERVAL PRECEDING AND CURRENT ROW
+        ) AS rolling_cycles_7d,
+        SUM(d.total_cycles) OVER (
+            ORDER BY d.sum_date::TIMESTAMP
+            RANGE BETWEEN '29 days'::INTERVAL PRECEDING AND CURRENT ROW
+        ) AS rolling_cycles_30d,
+        SUM(d.total_cycles) OVER (
+            ORDER BY d.sum_date::TIMESTAMP
+            RANGE BETWEEN '89 days'::INTERVAL PRECEDING AND CURRENT ROW
+        ) AS rolling_cycles_90d,
+        SUM(d.total_runtime) OVER (
+            ORDER BY d.sum_date::TIMESTAMP
+            RANGE BETWEEN '6 days'::INTERVAL PRECEDING AND CURRENT ROW
+        ) AS rolling_runtime_7d,
+        SUM(d.total_runtime) OVER (
+            ORDER BY d.sum_date::TIMESTAMP
+            RANGE BETWEEN '29 days'::INTERVAL PRECEDING AND CURRENT ROW
+        ) AS rolling_runtime_30d,
+        SUM(d.total_runtime) OVER (
+            ORDER BY d.sum_date::TIMESTAMP
+            RANGE BETWEEN '89 days'::INTERVAL PRECEDING AND CURRENT ROW
+        ) AS rolling_runtime_90d,
+        SUM(d.total_runtime) OVER (
+            ORDER BY d.sum_date::TIMESTAMP
+            RANGE BETWEEN '6 days'::INTERVAL PRECEDING AND CURRENT ROW
+        ) / SUM(d.total_cycles) OVER (
             ORDER BY d.sum_date::TIMESTAMP
             RANGE BETWEEN '6 days'::INTERVAL PRECEDING AND CURRENT ROW
         ) AS rolling_avg_cycle_time_7d,
-        AVG(d.avg_cycle_time) OVER (
+        SUM(d.total_runtime) OVER (
+            ORDER BY d.sum_date::TIMESTAMP
+            RANGE BETWEEN '29 days'::INTERVAL PRECEDING AND CURRENT ROW
+        ) / SUM(d.total_cycles) OVER (
             ORDER BY d.sum_date::TIMESTAMP
             RANGE BETWEEN '29 days'::INTERVAL PRECEDING AND CURRENT ROW
         ) AS rolling_avg_cycle_time_30d,
-        AVG(d.avg_cycle_time) OVER (
+        SUM(d.total_runtime) OVER (
+            ORDER BY d.sum_date::TIMESTAMP
+            RANGE BETWEEN '89 days'::INTERVAL PRECEDING AND CURRENT ROW
+        ) / SUM(d.total_cycles) OVER (
             ORDER BY d.sum_date::TIMESTAMP
             RANGE BETWEEN '89 days'::INTERVAL PRECEDING AND CURRENT ROW
         ) AS rolling_avg_cycle_time_90d,
+        (
+            SELECT
+                PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY dd.total_cycles)
+            FROM
+                daily dd
+            WHERE
+                dd.sum_date BETWEEN d.sum_date - '6 days'::INTERVAL AND d.sum_date
+        ) AS rolling_cycles_median_7d,
+        (
+            SELECT
+                PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY dd.total_cycles)
+            FROM
+                daily dd
+            WHERE
+                dd.sum_date BETWEEN d.sum_date - '29 days'::INTERVAL AND d.sum_date
+        ) AS rolling_cycles_median_30d,
+        (
+            SELECT
+                PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY dd.total_cycles)
+            FROM
+                daily dd
+            WHERE
+                dd.sum_date BETWEEN d.sum_date - '89 days'::INTERVAL AND d.sum_date
+        ) AS rolling_cycles_median_90d,
+        (
+            SELECT
+                PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY dd.total_runtime)
+            FROM
+                daily dd
+            WHERE
+                dd.sum_date BETWEEN d.sum_date - '6 days'::INTERVAL AND d.sum_date
+        ) AS rolling_runtime_median_7d,
+        (
+            SELECT
+                PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY dd.total_runtime)
+            FROM
+                daily dd
+            WHERE
+                dd.sum_date BETWEEN d.sum_date - '29 days'::INTERVAL AND d.sum_date
+        ) AS rolling_runtime_median_30d,
+        (
+            SELECT
+                PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY dd.total_runtime)
+            FROM
+                daily dd
+            WHERE
+                dd.sum_date BETWEEN d.sum_date - '89 days'::INTERVAL AND d.sum_date
+        ) AS rolling_runtime_median_90d,
+        (
+            SELECT
+                PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY cr.runtime)
+            FROM
+                cycle_runtimes cr
+            WHERE
+                cr.sum_date BETWEEN d.sum_date - '6 days'::INTERVAL AND d.sum_date
+        ) AS rolling_avg_cycle_time_median_7d,
+        (
+            SELECT
+                PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY cr.runtime)
+            FROM
+                cycle_runtimes cr
+            WHERE
+                cr.sum_date BETWEEN d.sum_date - '29 days'::INTERVAL AND d.sum_date
+        ) AS rolling_avg_cycle_time_median_30d,
         (
             SELECT
                 PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY cr.runtime)
@@ -54,7 +150,7 @@ CREATE OR REPLACE VIEW water_statistics.water_statistics_daily_cycle_view AS (
                 cycle_runtimes cr
             WHERE
                 cr.sum_date BETWEEN d.sum_date - '89 days'::INTERVAL AND d.sum_date
-        ) AS rolling_median_cycle_time_90d,
+        ) AS rolling_avg_cycle_time_median_90d,
         lm.median_cycle_time AS lifetime_median_cycle_time
     FROM
         daily d
