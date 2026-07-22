17733097195a6914cf697e379c1f59321fc46f65
Jesse Bishop <jessebishop@yahoo.com>
2026-07-22 14:53:02 -0400

    plotlim

--- plotting/generate_plot_water_cycle_time.R
+++ plotting/generate_plot_water_cycle_time.R
@@ -10,12 +10,13 @@ res <- dbGetQuery(con, query)
 query2 <- "SELECT SUM(total_runtime) / SUM(total_cycles) as overall_avg_cycle_time FROM water_statistics.water_statistics_daily_cycle_view;"
 res2 <- dbGetQuery(con, query2)
 
+plotmin <- floor(min(res$avg_cycle_time, res$rolling_avg_cycle_time_90d, res2$overall_avg_cycle_time, res$median_cycle_time, res$rolling_avg_cycle_time_median_90d, res$lifetime_median_cycle_time, na.rm=TRUE))
+plotmax <- ceiling(max(res$avg_cycle_time, res$rolling_avg_cycle_time_90d, res2$overall_avg_cycle_time, res$median_cycle_time, res$rolling_avg_cycle_time_median_90d, res$lifetime_median_cycle_time, na.rm=TRUE))
+vseq <- seq(plotmin, plotmax, 1)
+
 fname <- '/tmp/water_cycle_time.png'
 
 png(filename=fname, width=1024, height=400, units='px', pointsize=12, bg='white')
-plotmin <- floor(min(res$avg_cycle_time, res$rolling_avg_cycle_time_90d, res2$overall_avg_cycle_time, na.rm=TRUE))
-plotmax <- ceiling(max(res$avg_cycle_time, res$rolling_avg_cycle_time_90d, res2$overall_avg_cycle_time, na.rm=TRUE))
-vseq <- seq(plotmin, plotmax, 1)
 plot(res$sum_date, res$avg_cycle_time, type='l', col='white', ylim=c(plotmin, plotmax), yaxt='n', xlab="Date", ylab="Minutes", main="Avg Well Pump Cycle Time", lwd=3)
 axis(side=2, at=vseq, labels=vseq, las=1)
 abline(h=vseq, col='grey', lty=3)
@@ -30,9 +31,6 @@ system(paste("scp", fname, paste(paste(webuser, webhost, sep="@"), paste(webpath
 fname <- '/tmp/water_cycle_time_median.png'
 
 png(filename=fname, width=1024, height=400, units='px', pointsize=12, bg='white')
-plotmin <- floor(min(res$median_cycle_time, res$rolling_avg_cycle_time_median_90d, res$lifetime_median_cycle_time, na.rm=TRUE))
-plotmax <- ceiling(max(res$median_cycle_time, res$rolling_avg_cycle_time_median_90d, res$lifetime_median_cycle_time, na.rm=TRUE))
-vseq <- seq(plotmin, plotmax, 1)
 plot(res$sum_date, res$median_cycle_time, type='l', col='white', ylim=c(plotmin, plotmax), yaxt='n', xlab="Date", ylab="Minutes", main="Median Well Pump Cycle Time", lwd=3)
 axis(side=2, at=vseq, labels=vseq, las=1)
 abline(h=vseq, col='grey', lty=3)
