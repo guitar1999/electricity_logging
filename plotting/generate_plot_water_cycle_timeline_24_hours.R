cfe276c33502c1be7e183928b9bbf6bfd7f5ea46
Jesse Bishop <jessebishop@yahoo.com>
2026-07-23 11:37:31 -0400

    Make well pump cycle timeline compact

--- plotting/generate_plot_water_cycle_timeline_24_hours.R
+++ plotting/generate_plot_water_cycle_timeline_24_hours.R
@@ -3,33 +3,33 @@ if (! 'package:RPostgreSQL' %in% search()) {
     source(paste(Sys.getenv('HOME'), '/.rconfig.R', sep=''))
 }
 
-source(paste(githome, '/electricity_logging/plotting/water_cycle_plot_style.R', sep=''))
-
 query <- "WITH bounds AS (SELECT CURRENT_TIMESTAMP - INTERVAL '24 HOURS' AS start_time, CURRENT_TIMESTAMP AS end_time) SELECT GREATEST(wc.cycle_start, b.start_time) AS cycle_start, LEAST(wc.cycle_end, b.end_time) AS cycle_end, wc.runtime FROM water_statistics.water_cycles wc, bounds b WHERE wc.cycle_end >= b.start_time AND wc.cycle_start <= b.end_time ORDER BY wc.cycle_start;"
 res <- dbGetQuery(con, query)
 
 fname <- '/tmp/water_cycle_timeline_24_hours.png'
 mintime <- Sys.time() - 86400
 maxtime <- Sys.time()
-hseq <- seq(mintime, maxtime, 1800)
-hourseq <- seq(mintime, maxtime, 3600)
+hourseq <- seq(as.POSIXct(format(mintime, '%Y-%m-%d %H:00:00')), maxtime, by='hour')
 
-# Do some sunrise and sunset calculations
-today <- Sys.Date()
-query2 <- paste("SELECT (date || ' ' || sunrise)::timestamp AS sunrise, (date || ' ' || sunset)::timestamp AS sunset FROM astronomy_data WHERE date IN ('", today - 1, "', '", today, "') ORDER BY date;", sep="")
-res2 <- dbGetQuery(con, query2)
-
-
-cycle_plot_init(fname)
-cycle_draw_panel(c(mintime, maxtime), c(0, 1), "Time", "", "Well Pump Cycles - Last 24 Hours")
-cycle_grid_lines(v=hourseq)
-cycle_axis_time(hseq)
-abline(v=res2$sunrise, lty=2, col=cycle_orange)
-abline(v=res2$sunset, lty=2, col=cycle_orange)
+png(filename=fname, width=1024, height=160, units='px', pointsize=12, bg='#f8fafc')
+par(
+    bg='#f8fafc',
+    mar=c(2.5, 1, 2.2, 1),
+    mgp=c(1.6, 0.5, 0),
+    tcl=-0.25,
+    col.axis='#475569',
+    col.main='#0f172a',
+    fg='#475569',
+    bty='n'
+)
+plot(c(mintime, maxtime), c(0, 1), type='n', xlim=c(mintime, maxtime), ylim=c(0, 1), xlab="", ylab="", main="Well Pump Cycles - Last 24 Hours", xaxt='n', yaxt='n')
+rect(mintime, 0, maxtime, 1, col='#ffffff', border=NA)
+abline(v=hourseq, col='#e2e8f0')
+segments(mintime, 0.5, maxtime, 0.5, col='#cbd5e1', lwd=18, lend=1)
 if (nrow(res) > 0) {
-    segments(res$cycle_start, 0.5, res$cycle_end, 0.5, col=cycle_blue, lwd=10, lend=1)
+    segments(res$cycle_start, 0.5, res$cycle_end, 0.5, col='#2563eb', lwd=18, lend=1)
 }
-cycle_legend('topright', legend=c('Pump On'), col=c(cycle_blue), lty=c(1), lwd=c(8), inset=0.01)
+axis(side=1, at=hourseq, labels=format(hourseq, '%H'), col=NA, col.ticks='#475569', col.axis='#475569')
 dev.off()
 
 system(paste("scp", fname, paste(paste(webuser, webhost, sep="@"), paste(webpath, 'electricity2', sep="/"), sep=":"), sep=' '),ignore.stdout=TRUE,ignore.stderr=TRUE)
