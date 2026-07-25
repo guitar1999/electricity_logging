38787ed475b76639a5bca661ca4617bc2bdff138
Jesse Bishop <jessebishop@yahoo.com>
2026-07-25 07:57:34 -0400

    Use lifetime thresholds for pump runtime anomalies

--- plotting/generate_plot_water_cycle_anomalies_7_days.R
+++ plotting/generate_plot_water_cycle_anomalies_7_days.R
@@ -5,7 +5,7 @@ if (! 'package:RPostgreSQL' %in% search()) {
 
 source(paste(githome, '/electricity_logging/plotting/water_cycle_plot_style.R', sep=''))
 
-query <- "WITH thresholds AS (SELECT PERCENTILE_CONT(0.95) WITHIN GROUP (ORDER BY runtime) AS runtime_p95, PERCENTILE_CONT(0.05) WITHIN GROUP (ORDER BY runtime) AS runtime_p05 FROM water_statistics.water_cycles WHERE cycle_start >= CURRENT_TIMESTAMP - INTERVAL '90 DAYS') SELECT wc.cycle_start, wc.runtime, t.runtime_p05, t.runtime_p95 FROM water_statistics.water_cycles wc, thresholds t WHERE wc.cycle_start >= CURRENT_TIMESTAMP - INTERVAL '7 DAYS' ORDER BY wc.cycle_start;"
+query <- "WITH thresholds AS (SELECT PERCENTILE_CONT(0.95) WITHIN GROUP (ORDER BY runtime) AS runtime_p95, PERCENTILE_CONT(0.05) WITHIN GROUP (ORDER BY runtime) AS runtime_p05 FROM water_statistics.water_cycles) SELECT wc.cycle_start, wc.runtime, t.runtime_p05, t.runtime_p95 FROM water_statistics.water_cycles wc, thresholds t WHERE wc.cycle_start >= CURRENT_TIMESTAMP - INTERVAL '7 DAYS' ORDER BY wc.cycle_start;"
 res <- dbGetQuery(con, query)
 
 fname <- '/tmp/water_cycle_anomalies_7_days.png'
@@ -26,7 +26,7 @@ if (nrow(res) > 0) {
     abline(h=res$runtime_p05[1], col=cycle_gold, lty=2)
     points(res$cycle_start, res$runtime, col=pointcol, pch=19)
 }
-cycle_legend('topright', legend=c('Normal', '>= 90d p95', '<= 90d p05'), col=c(cycle_blue, cycle_red, cycle_gold), pch=c(19,19,19), inset=0.01)
+cycle_legend('topright', legend=c('Normal', '>= lifetime p95', '<= lifetime p05'), col=c(cycle_blue, cycle_red, cycle_gold), pch=c(19,19,19), inset=0.01)
 dev.off()
 
 system(paste("scp", fname, paste(paste(webuser, webhost, sep="@"), paste(webpath, 'electricity2', sep="/"), sep=":"), sep=' '),ignore.stdout=TRUE,ignore.stderr=TRUE)
